3b7c4eb399e1d4f51a005e7765d078912231e6d3
Trevor <tlow94@uw.edu>
2021-01-08 17:31:42 -0800

    students MVC

---
+++ db/migrate/20210109012119_create_students.rb
@@ -0,0 +1,9 @@
+class CreateStudents < ActiveRecord::Migration[6.0]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.string :password_digest
+      t.string :email
+    end
+  end
+end
