9e596142767de9e529ab1ff772a59f174dac5ecb
Trevor <tlow94@uw.edu>
2021-01-08 19:03:12 -0800

    jquery webpack nav bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,22 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-require ("materialize")
-
-
+// require("custom")
+require('jquery')
+ 
+import '../stylesheets/application'
+ 
+import 'materialize-css/dist/js/materialize'
+ 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+ 
+ 
+$(document).on('turbolinks:load', function() {
+  $('.sidenav').sidenav();
+  $(".dropdown-trigger").dropdown();
+});
