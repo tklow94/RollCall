9427df8c84ed94a674c1d0ebdbd95eb7c0ef7666
Trevor <tlow94@uw.edu>
2021-01-13 14:54:49 -0800

    validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ApplicationRecord
-    
+    validates :title, presence: true, length: {minimum: 5, maximum: 50}
+    validates :short_title, presence: true, length: {minimum: 3, maximum: 15}
+    validates :description, presence: true, length: {minimum: 5, maximum: 300}
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,10 @@
 class Student < ApplicationRecord
     has_secure_password
+    before_save { self.email = email.downcase }
+    validates :name, presence: true, length: {minimum: 1, maximum: 50}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i 
+    validates :email, presence: true, 
+                        uniqueness: { case_sensitive: false }, 
+                        length: { maximum: 101},
+                        format: { with: VALID_EMAIL_REGEX }
 end
