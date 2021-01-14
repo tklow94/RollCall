22f073964ca3e19e09045be737ae1cc8ba601ddb
Trevor <tlow94@uw.edu>
2021-01-13 17:20:21 -0800

    dry

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+    before_action :set_student, only: [:show,:edit,:update]
 
     def home
         @students = Student.all
@@ -8,18 +9,42 @@ class StudentsController < ApplicationController
         @student = Student.new
     end
 
+    def show
+        
+    end
+
+
     def create
         @student = Student.new(student_params)
         if @student.save
             flash[:notice] = "#{@student.name} has signed up"
-            redirect_to root_path
+            redirect_to @student
         else
             render 'new'
+        end
+    end
+
+    def edit
+       
+    end
+
+    def update
+      
+        if @student.update(student_params)
+            flash[:notice] = "Successfully updated profile"
+            redirect_to @student
+        else
+            flash[:notice] = "Failed to save changes to profile"
+            render 'edit'
         end
     end
     
 
     private
+
+    def set_student
+        @student = Student.find(params[:id])
+    end
 
     def student_params
         params.require(:student).permit(:name, :password, :email)
