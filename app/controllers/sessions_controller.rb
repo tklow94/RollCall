552ab0b2e743a96861736a58032d9312c536442c
Trevor <tlow94@uw.edu>
2021-01-14 17:27:19 -0800

    fix links and restrictions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+    skip_before_action :require_user, only: [:create, :new]
     def new
     end
 
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,7 @@
 class StudentsController < ApplicationController
     before_action :set_student, only: [:show,:edit,:update]
+    skip_before_action :require_user, only: [:new, :create]
+    before_action :require_same_student, only: [:edit, :update]
 
     def home
         @students = Student.all
@@ -48,6 +50,13 @@ class StudentsController < ApplicationController
 
     def student_params
         params.require(:student).permit(:name, :password, :email, :password_confirmation)
+    end
+
+    def require_same_student
+        if current_user != @student
+            flash[:notice] = "You can only edit your own profile"
+            redirect_to student_path(current_user)
+        end
     end
 
 end
